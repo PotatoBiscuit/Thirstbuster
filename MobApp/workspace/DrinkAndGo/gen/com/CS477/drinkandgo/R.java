51f3626029e43fa9b75748b7201787e86b87564a
toa5 <toa5@nau.edu>
2016-04-10 00:28:36 -0700

    Added code to represent database entities

--- MobApp/workspace/DrinkAndGo/gen/com/CS477/drinkandgo/R.java
+++ MobApp/workspace/DrinkAndGo/gen/com/CS477/drinkandgo/R.java
@@ -28,17 +28,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000a;
+        public static final int action_settings=0x7f08000d;
         public static final int card_number_view=0x7f080008;
         public static final int email_view=0x7f080006;
         public static final int name_view=0x7f080005;
         public static final int password_view=0x7f080003;
         public static final int password_view2=0x7f080007;
+        public static final int search_value=0x7f08000b;
         public static final int sign_in_button=0x7f080001;
         public static final int sign_up_button=0x7f080000;
         public static final int submit_button=0x7f080009;
         public static final int submit_view=0x7f080004;
+        public static final int title_search_venue=0x7f08000a;
         public static final int username_view=0x7f080002;
+        public static final int venue_list=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -63,6 +66,8 @@ public final class R {
         public static final int name=0x7f05000b;
         public static final int password=0x7f050008;
         public static final int password_error=0x7f05000f;
+        public static final int search=0x7f050012;
+        public static final int search_venue=0x7f050011;
         public static final int sign_in=0x7f050001;
         public static final int sign_up=0x7f050002;
         public static final int submit=0x7f050009;
