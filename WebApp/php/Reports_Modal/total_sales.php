d3a5aafb68d01d42408062a7afd8f87764883903
Sw33tT00th <jsl245@nau.edu>
2016-04-16 17:37:59 -0700

    added a chart to num_drinks report output, working on wait_time report

---
+++ WebApp/php/Reports_Modal/total_sales.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+if(!isset($_SESSION["ID"])){
+	echo 'Error in num_drinks.php, no session ID';
+	exit();
+}
+
+/*establish connection with the mySQL database*/
+$servername = $_SESSION["servername"];
+$username = $_SESSION["username"];
+$password = $_SESSION["password"];
+$dbname = $_SESSION["dbname"];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error){
+	echo 'Error in connecting to database';
+	die("Connection failed: " . $conn->connect_error);
+}
+
+?>
